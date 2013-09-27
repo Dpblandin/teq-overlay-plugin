fb06b973030dec4004d100e204f44239a99e5e70
Dpblandin <dav.pascaud@gmail.com>
2013-09-27 20:04:16 +0200

    Push master

--- README.md
+++ README.md
@@ -1,4 +1,183 @@
-teq-overlay-plugin
+Teq-overlay-plugin
 ==================
 
-Overlay plugin for teq
+Table of contents
+------------------
+
+* [Usage](#usage)
+* [Optional parameters](#optional-params)
+	* [Default settings](#default-settings)
+* [Callbacks](#callbacks)
+* [Advanced usage](#advanced-usage)
+	* [Accessing instanciated object](#accessing-the-instanciated-object)
+	* [Multi-instanciation](#multi-instanciation)
+	* [Custom load method](#custom-load-method)
+* [Examples](#examples)
+
+Usage 
+------------------
+
+Call the plugin with :
+
+	$([selector]).teqOverlay();
+
+
+[*To top*](#table-of-contents)
+
+Optional Parameters
+-------------------
+
+	overlayAttrs       : {object}
+Add any html attribute for the overlay container 
+
+	overlayCSS         : {object}
+Add css rules for the overlay container
+
+	overlayTransition :
+	{
+	type: string, 
+	duration: number in ms
+	}
+Add a jQuery transition effect for the overlay (fadeIn only currently supported)
+
+	contentContainer   : string || jQuery selector
+Specify with either a jQuery selector or an element tag which element should receive contents.
+If not specified, the plugin will look for any element that has `teq-overlay-content` as its id attribute.
+
+	addCloseBtn        : boolean
+If set to true, will add a close button to top right corner of the overlay content container
+
+	closeBtnAttrs      : {object}
+Add any html attribute for the close button element
+
+	closeBtnCSS        : {object}
+Add css rules for the close button element
+
+	closeBtnHtml       : string
+Html content of the close button element
+
+	closeTransition    : 
+	{	
+	type: string, 
+	duration: number in ms
+	}
+Add a jQuery transition effect for the close event (fadeOut only currently supported)
+
+	closeFromOutside : boolean
+If set to false, will disable the closing of the overlay by clicking anywhere outside of it
+
+[*To top*](#table-of-contents)
+
+### Default settings
+
+	settings : {
+				overlayAttrs: {
+					ID: 'overlay'
+				},
+				overlayCSS: {
+					display   : "none",
+					background: "rgba(0,0,0,.8)",
+					position  : "fixed",
+					top       : 0,
+					left      : 0,
+					width     : $(window).width(),
+					height    : $(window).height(),
+					zIndex    : 9999999
+				},
+				overlayTransition  : {
+					type: "show", 
+					duration: 0
+				},
+				contentContainer : $('div#teq-overlay-content'),
+				addCloseBtn: false,
+				closeBtnAttrs:{
+					ID: 'overlay-close'
+				},
+				closeBtnCSS: {
+					position: "absolute",
+					top      : 0,
+					right    : 0,
+					cursor   : "pointer"
+				},
+				closeBtnHtml: "x",
+				closeTransition: {
+					type: "hide",
+					duration: 0
+				},
+				closeFromOutside : true
+			},
+
+[*To top*](#table-of-contents)
+
+Callbacks
+------------------
+
+When calling the plugin, you can pass any of the following callback methods :
+
+* `beforeOpenCB`  : called *before* the overlay opens
+* `afterOpenCB`   : called *after* the overlay and it's content is loaded
+* `beforeCloseCB` : called *before* the overlay closes
+* `afterCloseCB`  : called *after* the overlay is closed
+
+[*To top*](#table-of-contents)
+
+Advanced usage
+------------------
+### Accessing the instanciated object
+
+When you call the plugin with `$("mySelector").teqOverlay()`, you can quickly access the teqOverlay object with :
+
+	$("mySelector").data('teqOverlay');
+
+This will return an object containing :
+
+* `$element` : the jQuery element
+* `element`  : the javascript element 
+* `settings` : an object containing all the parameters, including any callbacks you have passed in.
+
+### Multi-instanciation
+
+You can of course call the plugin multiple times on different elements like this :
+
+	$('a.overlay').teqOverlay();
+
+	$('button[data-overlay="true"]').teqOverlay({
+		addCloseBtn: false,
+		contentContainer: $('#overlay-content'),
+		afterCloseCB: function() {}
+	});
+
+	// etc.
+
+### Custom load method
+
+If you wish to use your own method for loading content, you can pass the `loadContent` method as a parameter, like so:
+
+	$("myselector").teqOverlay({
+		//optional params ...
+		loadContent : function(){
+			// Your method logic here
+		}
+	       
+	});
+
+Please note that as of yet, `jQuery.load()` is not fully supported and may produce unwanted behaviours.
+
+[*To top*](#table-of-contents)
+
+Examples
+-------------------
+
+    $('a[data-overlay]').teqOverlay({
+      onInitCB: function(){
+        alert("onInit callback called");
+      },
+      overlayCSS:{backgroundColor: "#F300", zIndex: 99999},
+      overlayTransition : {type: "fadeIn", duration:500},
+      contentContainer: $('#overlay-content'),
+      addCloseBtn : true,
+      closeBtnHtml: '<span class="close btn">close me</span>',
+      closeFromOutside : false
+    });
+
+[*To top*](#table-of-contents)
---
+++ js/overlay-0.1.js
@@ -0,0 +1,320 @@
+var teqOverlay = {
+		/* CB (callback) methods */
+			onInitCB: function(){},
+
+			beforeOpenCB: function(){},
+
+			onOpenCB: function(){},
+
+			afteropenCB: function(){},
+
+			beforeCloseCB: function(){},
+
+			afterCloseCB: function(){},
+
+			loadContent: function(){},
+
+			/* Init method */
+			init: function(options, element){
+				this.settings = $.extend(true, {}, this.settings, options );
+				this.element = element;
+				this.$element = $(element);
+				var done = false;
+
+				/* Check if contentContainer param is passed as a simple string or a selector */
+
+				if(typeof this.settings.contentContainer == 'string' || this.settings.contentContainer instanceof String){
+					this.settings.contentContainer = $(this.settings.contentContainer);
+				}
+
+				/* Abort if no element matches selector */
+				if(this.settings.contentContainer.length == 0)
+				{
+					window.console.log && console.log("Error : jQuery couldn't find the content container specified with the selector you used ("+this.settings.contentContainer.selector+')');
+					return false;
+				}
+
+				/* If an element is found, add background overlay and bind click events for links */
+				if(this.$element.length > 0){
+					this._bindClickEvents();
+					done = !done;
+				}
+				else{
+					window.console.log && console.log("Error : jQuery couldn't find the element specified with the selector you used ("+element.selector+')');
+					return false;
+				}
+
+				/* When all is done, call callback */
+				//done ? this.settings.onInitCB() : '';
+
+				return this;
+			},
+
+			/* Settings */
+
+		settings : {
+			overlayAttrs: {
+				ID: 'overlay'
+			},
+			overlayCSS: {
+				display   : "none",
+				background: "rgba(0,0,0,.8)",
+				position  : "fixed",
+				top       : 0,
+				left      : 0,
+				width     : $(window).width(),
+				height    : $(window).height(),
+				zIndex    : 9999999
+			},
+			overlayTransition  : {
+				type: "show", 
+				duration: 0
+			},
+			contentContainer : $('div#teq-overlay-content'),
+			addCloseBtn: false,
+			closeBtnAttrs:{
+				ID: 'overlay-close'
+			},
+			closeBtnCSS: {
+				position: "absolute",
+				top      : 0,
+				right    : 0,
+				cursor   : "pointer"
+			},
+			closeBtnHtml: "x",
+			closeTransition: {
+				type: "hide",
+				duration: 0
+			},
+			closeFromOutside : true
+		},
+
+			/* Bind click events to matched elements to show overlay content */
+			_bindClickEvents: function(){
+				window.console.log && console.log('binding click events...');
+				var this_instance = this;
+				this.$element.bind("click", function(e) {
+					e.stopPropagation();
+					e.preventDefault();
+					/* Add overlay to DOM and return true if all is good */
+					if(this_instance.addOverlayToStage()) {
+						/* Pass href element attribute to loading method */
+						this_instance.loadOverlayContent($(this).attr('href'));
+
+						/* Show overlay with specified transitions and then fire afterOpenCB callback */
+						if( this_instance.showOverlayWithTransition($('#'+this_instance.settings.overlayAttrs.ID)) ) {
+
+							if(typeof this_instance.settings.afterOpenCB == 'function') {
+
+								this_instance.settings.afterOpenCB.call(this_instance);
+							}
+						}	
+					}							
+				});
+				window.console.log && console.log('------------DONE---------------');
+			},
+
+			/* Add background overlay */
+			addOverlayToStage: function(){
+				window.console.log && console.log('Adding overlay to stage...');
+				window.console.log && console.log(this);
+				
+				var isDone = false;
+				this.cleanContents();
+				/* Check wether a background has already beend added or not */
+				if(this.settings.contentContainer.parents("#"+this.settings.overlayAttrs.ID).length == 0)
+				{
+					$.when(this.settings.contentContainer.wrap(
+						$('<div/>')
+						.attr(this.settings.overlayAttrs)
+						.css(this.settings.overlayCSS)
+					)).done(function(){
+						isDone = true;
+					});
+	
+				}else {
+
+					this.settings.contentContainer.parents("#"+this.settings.overlayAttrs.ID).css(this.settings.overlayCSS)
+					.done(function(){isDone = true});
+				}
+
+				return isDone;
+			
+				window.console.log && console.log('------------DONE---------------');
+			},
+
+			/* Load overlay content into content container, checks wether to add a close button or not */
+			loadOverlayContent: function( hrefLink ) {
+				var this_instance = this;
+				var done = false;
+				if(typeof this_instance.settings.beforeOpenCB == 'function') {
+
+					this_instance.settings.beforeOpenCB.call(this_instance);
+				}
+				window.console.log && console.log("loading content...");
+				
+
+				if(typeof this_instance.settings.loadContent === 'function') {
+					
+					this_instance.settings.loadContent();
+
+					if(this_instance.settings.addCloseBtn) {
+						this_instance.addCloseButton();	
+					}
+					if(this_instance.settings.closeFromOutside) {
+						this_instance.addDocumentEventHandler(); 
+					}
+
+					done = true;
+
+				}
+				else {
+					this_instance.settings.contentContainer.load(hrefLink, function() {
+
+						if(this_instance.settings.addCloseBtn){
+							this_instance.addCloseButton();	
+						}
+						if(this_instance.settings.closeFromOutside){
+
+							this_instance.addDocumentEventHandler(); 
+						}
+						done = true;
+					});
+				}
+
+				window.console.log && console.log('------------DONE---------------');
+			},
+
+			cleanContents: function() {
+				var elements = $("*").filter(function() {
+				    return $(this).data("teqOverlay") !== undefined;
+				});
+
+				if(elements.length > 0){
+
+					for(var i = 0; i < elements.get().length; i++){
+
+						$(elements.get(i)).data("teqOverlay").settings.contentContainer.unwrap().empty();
+					}
+						
+				}
+			},
+
+			/* Add a close button to overlay if necessary, then bind click event on that button in order to hide overlay */
+			addCloseButton: function() {
+				var this_instance = this;
+				window.console.log && console.log("adding close btn...");
+				if($("#"+this.settings.closeBtnAttrs.ID).length == 0 ){
+
+					var overlayContainer = $("#"+this.settings.overlayAttrs.ID);
+
+					$('<div/>')
+					.attr(this.settings.closeBtnAttrs)
+					.css(this.settings.closeBtnCSS)
+					.html(this.settings.closeBtnHtml)
+					.prependTo(this.settings.contentContainer)
+					.bind( "click", this.closeButtonHandler(overlayContainer) )
+
+					this.settings.contentContainer.bind("click", function(e){
+						e.stopPropagation();
+					})
+				}
+				window.console.log && console.log('------------DONE---------------');
+			},
+
+			addDocumentEventHandler: function() {
+				var overlayContainer = $("#"+this.settings.overlayAttrs.ID);
+				$(document).bind("click.outsideClickEvent", this.outsideCloseHandler(overlayContainer));
+						
+			},
+
+			removeDocumentEventHandler: function() {
+				$(document).unbind("click.outsideClickEvent");
+			},
+
+			outsideCloseHandler: function(overlayContainer){
+
+				var this_instance = this;
+				return function(e) {
+					e.stopPropagation();
+					e.preventDefault();
+					this_instance.hideElementWithTransition(overlayContainer, this_instance.settings.closeTransition);
+				}
+			},
+			closeButtonHandler: function (overlayContainer){
+				var this_instance = this;
+				return function(e) {
+					e.stopPropagation();
+					e.preventDefault();
+					this_instance.hideElementWithTransition(overlayContainer, this_instance.settings.closeTransition);
+				}
+			},
+
+			/* Hides an element with specified transition effect */
+			hideElementWithTransition: function( element, transition ){
+				if(typeof this.settings.beforeCloseCB == 'function') {
+
+					this.settings.beforeCloseCB.call(this);
+				}
+				switch (transition.type) {
+
+						case "fadeOut":
+							element.fadeOut(transition.duration);
+						break;
+
+						default:
+							element.hide();
+						break;
+						
+				}
+
+				this.removeDocumentEventHandler();
+
+				if(typeof this.settings.afterCloseCB == 'function') {
+
+					this.settings.afterCloseCB.call(this);
+				}
+			},
+
+			/* Shows overlay with specified transition effect */
+			showOverlayWithTransition: function ( element ){
+				var done = false;
+					switch (this.settings.overlayTransition.type) {
+
+						case "fadeIn":
+							element.fadeIn(this.settings.overlayTransition.duration, function(){
+								done = true;
+							});
+						break;
+
+						default:
+							element.show(0, function() {
+								done = true;
+							});
+						break;
+						
+					}
+				
+				return done;
+			}
+		
+};
+
+
+(function( $ ) {
+ 
+    $.fn.teqOverlay = function( options ) {	
+       
+       	return this.each(function () {
+
+       		var aTeqOverlay = Object.create(teqOverlay);
+
+       		aTeqOverlay.init(options, this)
+       		
+       		$.data(this, 'teqOverlay', aTeqOverlay);
+       	});
+
+
+    };
+ 
+})( jQuery );
